7908d7e775bf9a56d5fd8a4b98815696c0e10e8f
Md Repon <repon7253@gmail.com>
2025-07-10 01:46:45 +0600

    setup complited

---
+++ src/config/env.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+interface EnvConfig {
+    PORT: string,
+    DB_URL: string,
+    NODE_ENV: "development" | "production"
+}
+
+const loadEnvVariables = (): EnvConfig => {
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV"]
+    requiredEnvVariables.forEach(key => {
+        if (!process.env[key]) {
+            throw new Error(`Missing require environment variable ${key}`)
+        }
+    })
+
+    return {
+        PORT: process.env.PORT!,
+        DB_URL: process.env.DB_URL!,
+        NODE_ENV: process.env.NODE_ENV as "development" | "production"
+    }
+}
+export const envVars: EnvConfig = loadEnvVariables()
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,19 @@
 import { Server } from 'http'
 import mongoose from 'mongoose';
 import app from './app';
+import { envVars } from './config/env';
 
 let server: Server;
 
 
 const startServer = async () => {
     try {
-        await mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.lpi7o.mongodb.net/tour-management-backend?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(envVars.DB_URL)
 
         console.log('connected to DB!')
 
-        server = app.listen(5000, () => {
-            console.log('Server is listening to port 5000')
+        server = app.listen(envVars.PORT, () => {
+            console.log(`Server is listening to port ${envVars.PORT}`)
         })
     } catch (error) {
         console.log(error)
