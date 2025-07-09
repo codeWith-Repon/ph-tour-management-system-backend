79e7cf8776b45bc715e72beaa16a59a526b5591c
Md Repon <repon7253@gmail.com>
2025-07-10 01:15:04 +0600

    eslint setup and add error handleing

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Server } from 'http'
 import mongoose from 'mongoose';
 import app from './app';
@@ -11,7 +12,7 @@ const startServer = async () => {
 
         console.log('connected to DB!')
 
-       server = app.listen(5000, () => {
+        server = app.listen(5000, () => {
             console.log('Server is listening to port 5000')
         })
     } catch (error) {
@@ -20,3 +21,66 @@ const startServer = async () => {
 }
 
 startServer()
+
+process.on('unhandledRejection', (error) => {
+    console.log("Unhandled Rejection detected... server shutting down..", error);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+//unhandle rejection error
+// Promise.reject(new Error("I forgot to catch this promise"))
+
+
+process.on('uncaughtException', (error) => {
+    console.log("Uncaught Ecception detected... server shutting down..", error);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+//uncaught exception error
+// console.log(a)
+// throw new Error("I forgot to handle this local error")
+
+
+process.on('SIGTERM', () => {
+    console.log("SIGTERM signal recieved... server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+process.on('SIGINT', () => {
+    console.log("SIGINT signal recieved... server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+
+/**
+ * unhandled rejection error
+ * uncaught rejection error
+ * signal termination sigterm
+ */
