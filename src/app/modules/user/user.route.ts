39372c8fe63a674338da733a22c744edee297912
Md Repon <repon7253@gmail.com>
2025-07-10 22:56:31 +0600

    feat: add validateRequest middleware and update user routes to use it; add updateUserZodSchema for user validation

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,29 +1,9 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { UserControllers } from "./user.controller";
-import { AnyZodObject } from "zod";
 import { createUserZodSchema } from "./user.validation";
-
-const validateRequest = (zodSchema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        req.body = await zodSchema.parseAsync(req.body)
-        next()
-    } catch (error) {
-        next(error)
-    }
-}
+import { validateRequest } from "../../middlewares/validateRequest";
 
 const router = Router()
-
-// router.post('/register', async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//         req.body = await createUserZodSchema.parseAsync(req.body)
-//         next()
-//     } catch (error) {
-//         next(error)
-//     }
-// },
-//     UserControllers.createUser
-// )
 
 router.post('/register', validateRequest(createUserZodSchema), UserControllers.createUser)
 router.get('/all-users', UserControllers.getAllUsers)
