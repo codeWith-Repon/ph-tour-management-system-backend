0149f34c7a5d22ba54d75aa46707549a027159e0
Md Repon <repon7253@gmail.com>
2025-07-10 22:31:26 +0600

    feat: add user validation schema and refactor registration route to use it

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,10 +1,31 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "./user.controller";
+import { AnyZodObject } from "zod";
+import { createUserZodSchema } from "./user.validation";
 
+const validateRequest = (zodSchema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        req.body = await zodSchema.parseAsync(req.body)
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
 
 const router = Router()
 
-router.post('/register', UserControllers.createUser)
+// router.post('/register', async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//         req.body = await createUserZodSchema.parseAsync(req.body)
+//         next()
+//     } catch (error) {
+//         next(error)
+//     }
+// },
+//     UserControllers.createUser
+// )
+
+router.post('/register', validateRequest(createUserZodSchema), UserControllers.createUser)
 router.get('/all-users', UserControllers.getAllUsers)
 
 export const UserRoutes = router
