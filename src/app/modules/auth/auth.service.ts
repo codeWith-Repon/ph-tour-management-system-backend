9f34546db427d9b3948e7ee09fd9b2b7514d975d
Md Repon <repon7253@gmail.com>
2025-07-11 11:57:22 +0600

    feat: implement JWT authentication for user login and authorization in user routes

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { IUser } from "../user/user.interface"
 import { User } from "../user/user.model";
 import httpStatus from 'http-status'
 import bcryptjs from 'bcryptjs'
+import jwt from "jsonwebtoken"
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
     const { email, password } = payload;
@@ -19,8 +20,17 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
         throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
     }
 
+    const jwtPayload = {
+        userId: isUserExist._id,
+        email: isUserExist.email,
+        role: isUserExist.role
+    }
+    const accessToken = jwt.sign(jwtPayload, "secret", {
+        expiresIn: "1d"
+    })
+
     return {
-        email: isUserExist.email
+        accessToken
     }
 }
 
