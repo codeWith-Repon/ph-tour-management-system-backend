2da3c3cba06f12a77a1b2110eaf60c5c98398768
Md Repon <repon7253@gmail.com>
2025-07-11 15:23:17 +0600

    feat: implement checkAuth middleware for role-based access control in user routes

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,37 +1,13 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { UserControllers } from "./user.controller";
 import { createUserZodSchema } from "./user.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
-import AppError from "../../errorHelpers/AppError";
-import { JwtPayload } from "jsonwebtoken"
-import { verifyToken } from "../../utils/jwt";
-import { envVars } from "../../../config/env";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "./user.interface";
 
 const router = Router()
 
-
-const checkAuth = (...authRoles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const accessToken = req.headers.authorization;
-
-        if (!accessToken) {
-            throw new AppError(403, "No Token Recived")
-        }
-
-        const verifiedToken = verifyToken(accessToken, envVars.JWT_ACCESS_SECRET) as JwtPayload;
-
-        if (!authRoles.includes(verifiedToken.role)) {
-            throw new AppError(403, "You are not permitted to view this route!!")
-        }
-
-        next()
-
-    } catch (error) {
-        next(error)
-    }
-}
-
 router.post('/register', validateRequest(createUserZodSchema), UserControllers.createUser)
-router.get('/all-users', checkAuth("ADMIN"), UserControllers.getAllUsers)
+router.get('/all-users', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers)
 
 export const UserRoutes = router
