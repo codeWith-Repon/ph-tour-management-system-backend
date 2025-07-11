8d0940d8500b4787ede74b9874be7939851b7fa7
Md Repon <repon7253@gmail.com>
2025-07-11 17:39:04 +0600

    add seedSuperAdmin utility for initial setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -9,11 +9,22 @@ interface EnvConfig {
     NODE_ENV: "development" | "production",
     JWT_ACCESS_SECRET: string,
     JWT_ACCESS_EXPIRES: string,
-    BCRYPT_SALT_ROUND: string
+    BCRYPT_SALT_ROUND: string,
+    SUPER_ADMIN_EMAIL: string,
+    SUPER_ADMIN_PASSWORD: string
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET"]
+    const requiredEnvVariables: string[] = [
+        "PORT",
+        "DB_URL",
+        "NODE_ENV",
+        "BCRYPT_SALT_ROUND",
+        "JWT_ACCESS_EXPIRES",
+        "JWT_ACCESS_SECRET",
+        "SUPER_ADMIN_EMAIL",
+        "SUPER_ADMIN_PASSWORD"
+    ];
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
             throw new Error(`Missing require environment variable ${key}`)
@@ -26,7 +37,9 @@ const loadEnvVariables = (): EnvConfig => {
         NODE_ENV: process.env.NODE_ENV as "development" | "production",
         JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET!,
         JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES!,
-        BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND!
+        BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND!,
+        SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL!,
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD!
     }
 }
 export const envVars: EnvConfig = loadEnvVariables()
