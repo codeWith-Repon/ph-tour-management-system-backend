617dbdf7bee335013cebf0429600aa882890d078
Md Repon <repon7253@gmail.com>
2025-07-16 10:31:35 +0600

    handle mongoose error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -8,8 +8,34 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
 
     let statusCode = 500
     let message = "Something Went Wrong!!"
+    const errorSources: any = [
+        // {
+        //     path: "isDeleted",
+        //     message: "Cast Failed"
+        // }
+    ]
 
-    if (err instanceof AppError) {
+    // duplicate error
+    if (err.code === 11000) {
+        const matchedArray = err.message.match(/"([^"]*)"/)
+        statusCode = 400;
+        message = `${matchedArray[1]} already exists!!`
+    }
+    // cast error or invalid Objectid
+    else if (err.name === "CastError") {
+        statusCode = 400;
+        message = "Invalid MongoDB objectID. Please provide a valid id"
+    }
+    else if (err.name === "ValidationError") {
+        statusCode = 400;
+        const errors = Object.values(err.errors)
+        errors.forEach((errorObject: any) => errorSources.push({
+            path: errorObject.path,
+            message: errorObject.message
+        }))
+        message = 'Validation Error'
+    }
+    else if (err instanceof AppError) {
         statusCode = err.statusCode
         message = err.message
     } else if (err instanceof Error) {
@@ -20,7 +46,7 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
     res.status(statusCode).json({
         success: false,
         message,
-        err,
+        errorSources,
         stack: envVars.NODE_ENV === 'development' ? err.stack : null
     })
 }
