aa7c4231c4b9f3c8d710d737407de653bc15e4b7
Md Repon <repon7253@gmail.com>
2025-07-18 03:38:03 +0600

    feat: tourType, Tour, division functionality

--- src/app/modules/division/division.controller.ts
+++ src/app/modules/division/division.controller.ts
@@ -5,7 +5,7 @@ import { sendResponse } from "../../utils/sendResponse";
 
 
 const createDivision = catchAsync(async (req: Request, res: Response) => {
-   
+
     const result = await DivisionService.createDivision(req.body)
     sendResponse(res, {
         statusCode: 201,
@@ -15,7 +15,58 @@ const createDivision = catchAsync(async (req: Request, res: Response) => {
     })
 })
 
+const getAllDivisions = catchAsync(async (req: Request, res: Response) => {
+    const result = await DivisionService.getAllDivisions()
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Divisions retrieved",
+        data: result.data,
+        meta: result.meta
+    })
+})
+
+const getSingleDivision = catchAsync(async (req: Request, res: Response) => {
+    const { slug } = req.params
+    const result = await DivisionService.getSingleDivision(slug)
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Divisions retrieved",
+        data: result.data,
+    })
+})
+
+const updateDivision = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params
+    const result = await DivisionService.updateDivision(id, req.body)
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Division updated successfully",
+        data: result,
+    })
+})
+
+const deleteDivision = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params
+    const result = await DivisionService.deleteDivision(id)
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Divisions deleted successfully",
+        data: result,
+    })
+})
 
 export const DivisionController = {
-    createDivision
+    createDivision,
+    getAllDivisions,
+    getSingleDivision,
+    updateDivision,
+    deleteDivision
 }
--- src/app/modules/division/division.route.ts
+++ src/app/modules/division/division.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { createDivisionSchema } from "./division.validation";
+import { createDivisionSchema, updateDivisionSchema } from "./division.validation";
 import { DivisionController } from "./division.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "../user/user.interface";
@@ -14,5 +14,17 @@ router.post(
     validateRequest(createDivisionSchema),
     DivisionController.createDivision
 )
-
+router.get("/", DivisionController.getAllDivisions)
+router.get("/:slug", DivisionController.getSingleDivision)
+router.patch(
+    "/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    validateRequest(updateDivisionSchema),
+    DivisionController.updateDivision
+)
+router.delete(
+    "/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    DivisionController.deleteDivision
+)
 export const DivisionRoutes = router
