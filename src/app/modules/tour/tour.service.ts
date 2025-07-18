b1c7f7173c4b86a923e7cb015d4219ee8294d659
Md Repon <repon7253@gmail.com>
2025-07-18 17:08:21 +0600

    𝒑𝒂𝒈𝒊𝒏𝒂𝒕𝒊𝒐𝒏 𝒘𝒊𝒕𝒉 𝒔𝒌𝒊𝒑 𝒂𝒏𝒅 𝒍𝒊𝒎𝒊𝒕

--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -74,7 +74,10 @@ const getAllTours = async (query: Record<string, string>) => {
     const filter = query;
     const searchTerm = query.searchTerm || "";
     const sort = query.sort || "-createdAt";
-    const fields = query.fields.split(",").join(" ") || ""
+    const fields = query.fields?.split(",").join(" ") || ""
+    const page = Number(query.page) || 1;
+    const limit = Number(query.limit) || 10;
+    const skip = (page - 1) * limit
 
     // delete filter["searchTerm"]
     // delete filter["sort"]
@@ -102,7 +105,13 @@ const getAllTours = async (query: Record<string, string>) => {
 
     // })
 
-    const tours = await Tour.find(searchQuery).find(filter).sort(sort).select(fields);
+    const tours = await Tour
+        .find(searchQuery).
+        find(filter)
+        .sort(sort)
+        .select(fields)
+        .skip(skip)
+        .limit(limit);
     const totalTours = await Tour.countDocuments()
 
     return {
