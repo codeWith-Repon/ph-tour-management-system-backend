004bbba72399a07d76c9a6b95c21badacd3662d2
Md Repon <repon7253@gmail.com>
2025-07-26 08:58:41 +0600

    🔐✨ OTP Verification System via Email 🔁📨
    ✅ Added OTP generation & email delivery using crypto + Redis
    📬 Integrated email template: `otp.ejs` with user name & code
    🧠 Smart validations:
       - User existence check ❌
       - Already verified protection 🛡️
       - OTP match & expiry via Redis ⏰
    
    🧰 Created:
      - 📦 OTPService (sendOTP, verifyOTP)
      - 🎯 OTPController (sendOTP, verifyOTP endpoints)
    
    💥 Ensured clean Redis deletion & user verification on success
    📢 Message feedback: "OTP sent" and "OTP verified" 🚀
    
    #2FA #RedisOTP #SecureSignup 🔒

--- src/app/modules/otp/otp.controller.ts
+++ src/app/modules/otp/otp.controller.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import { OTPService } from "./otp.service";
 
 
 const sendOTP = catchAsync(async (req: Request, res: Response) => {
+    const { email, name } = req.body
+    await OTPService.sendOTP(email, name)
+
     sendResponse(res, {
         statusCode: 200,
         success: true,
@@ -13,6 +17,10 @@ const sendOTP = catchAsync(async (req: Request, res: Response) => {
 })
 
 const verifyOTP = catchAsync(async (req: Request, res: Response) => {
+    const { email, otp } = req.body
+
+    await OTPService.verifyOTP(email, otp)
+
     sendResponse(res, {
         statusCode: 200,
         success: true,
